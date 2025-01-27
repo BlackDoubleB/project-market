8097c4127de19c793c1e20c7acc4a0667f52c6ee
BlackDoubleB <blancablacido@gmail.com>
2025-01-26 23:42:19 -0500

    añadiendo data a la bd

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,46 +1,45 @@
 import { v4 as uuidv4 } from 'uuid';
-// eslint-disable-next-line prefer-const
-let myuuid = uuidv4();
 const date: Date = new Date();
+
 
 const categories = [
     {
-        id: 'b995425f-cebb-4f68-b505-08d2168e7e72',
+        id: uuidv4(),
         name: 'Shoes',
     },
     {
-        id: '3533c876-4494-444d-b3cd-ac91e723e89d',
+        id: uuidv4(),
         name: 'Clothes',
     },
     {
-        id: '59475b1d-35bd-4369-8cde-5419e4e8edf9',
+        id: uuidv4(),
         name: 'Accessories',
     },
     {
-        id: 'f0638f0e-6196-468c-b957-2e9f4332c2e0',
+        id: uuidv4(),
         name: 'Electronics',
     },
     {
-        id: '340ad912-456d-41a3-9fc7-6475662b468a',
+        id: uuidv4(),
         name: 'Books',
     },
     {
-        id: 'f145a9f4-65d8-4061-a513-d3bab3727d54',
+        id: uuidv4(),
         name: 'Home',
     }
 ];
 
 const products = [
     {
-        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+        id: uuidv4(),
         id_category: categories[0].id,
         name: 'Sandalias Casuales Footloose Mujeres Fch-Nz006 Marie Delux',
         image_url: `/products/SandaliasCasualesFootlooseMujeresFch-Nz006MarieDelux.png`,
-        price: '20',
+        price: 20,
         date: date
     },
     {
-        id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+        id: uuidv4(),
         id_category: categories[1].id,
         name: 'Sueter de punto TRAF Color Blanco',
         image_url: '/products/SueterdepuntoTRAFColorBlanco.png',
@@ -48,68 +47,68 @@ const products = [
         date: date
     },
     {
-        id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
+        id: uuidv4(),
         id_category: categories[2].id,
         name: 'Reloj Curren Krec57180303 Azul Hombre',
         image_url: '/products/RelojCurrenKrec57180303AzulHombre.png',
-        price: '500',
+        price: 500,
         date: date
     },
     {
-        id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        id: uuidv4(),
         id_category: categories[3].id,
         name: 'Auriculares Bluetooth QCY H3 ANC 70Hrs con cancelacion de ruido Blanco',
         image_url: '/products/AuricularesBluetoothQCYH3ANC70HrsconcancelacionderuidoBlanco.png',
-        price: '150',
+        price: 150,
         date: date
     },
     {
-        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
+        id:uuidv4(),
         id_category: categories[4].id,
         name: 'Libro de Hechizos Harry Potter',
         image_url: '/products/LibrodeHechizosHarryPotter.png',
-        price: '250',
+        price: 250,
         date: date
     },
     {
-        id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
+        id: uuidv4(),
         id_category: categories[5].id,
         name: 'Sofa 2 Cuerpos Moscu Hys',
         image_url: '/products/Sofa2CuerposMoscuHys.png',
-        price: '2500',
+        price: 2500,
         date: date
     }
 ];
 
 const stock = [
     {
-        id: myuuid,
+        id: uuidv4(),
         id_product: products[0].id,
         quantity: 10,
         date: date,
     },
     {
-        id: myuuid,
+        id: uuidv4(),
         id_product: products[1].id,
         quantity: 10,
         date: date,
     }, {
-        id: myuuid,
+        id: uuidv4(),
         id_product: products[2].id,
         quantity: 10,
         date: date,
     }, {
-        id: myuuid,
+        id: uuidv4(),
         id_product: products[3].id,
         quantity: 10,
         date: date,
     }, {
-        id: myuuid,
+        id: uuidv4(),
         id_product: products[4].id,
         quantity: 10,
         date: date,
     }, {
-        id: myuuid,
+        id: uuidv4(),
         id_product: products[5].id,
         quantity: 10,
         date: date,
@@ -118,14 +117,14 @@ const stock = [
 
 const role = [
     {
-        id: myuuid,
+        id: uuidv4(),
         name: 'admin'
     }
 ]
 
 const people = [
     {
-        id: myuuid,
+        id: uuidv4(),
         dni: 77149964,
         name: "blanca",
         lastname: 'blacido aparicio',
@@ -135,10 +134,10 @@ const people = [
 
 const users = [
     {
-        id: myuuid,
+        id: uuidv4(),
         id_role: role[0].id,
         id_people: people[0].id,
-        user: 'User',
+        user_name: 'User',
         password: '123456',
         date_login: date,
     },
@@ -146,95 +145,80 @@ const users = [
 
 const sales = [
     {
-        id: myuuid,
+        id: uuidv4(),
         id_user: users[0].id,
         method: 'card',
-        date: '2022-12-06',
+        date: date,
         total: 15795,
     },
     {
-        id: myuuid,
+        id: uuidv4(),
         id_user: users[0].id,
         method: 'card',
-        date: '2022-11-14',
+        date: date,
         total: 20348,
     },
     {
-        id: myuuid,
+        id: uuidv4(),
         id_user: users[0].id,
         method: 'cash',
-        date: '2022-10-29',
+        date: date,
         total: 3040,
     },
     {
-        id: myuuid,
+        id: uuidv4(),
         id_user: users[0].id,
         method: 'cash',
-        date: '2023-09-10',
+        date: date,
         total: 4480,
     },
     {
-        id: myuuid,
+        id: uuidv4(),
         id_user: users[0].id,
         method: 'card',
-        date: '2023-08-05',
+        date: date,
         total: 34577,
     },
     {
-        id: myuuid,
+        id: uuidv4(),
         id_user: users[0].id,
         method: 'card',
-        date: '2023-07-16',
+        date: date,
         total: 54246,
     }
 ];
 
-const detail_sale_product = [
+const detail_sale_products = [
     {
         id_sale:sales[0].id,
         id_product:products[0].id,
-        cantidad: 1,
+        quantity: 1,
     },
     {
         id_sale:sales[1].id,
         id_product:products[1].id,
-        cantidad: 1,
+        quantity: 1,
     },
     {
         id_sale:sales[2].id,
         id_product:products[2].id,
-        cantidad: 1,
+        quantity: 1,
     },
     {
         id_sale:sales[3].id,
         id_product:products[3].id,
-        cantidad: 1,
+        quantity: 1,
     },
     {
         id_sale:sales[4].id,
         id_product:products[4].id,
-        cantidad: 1,
+        quantity: 1,
     },
     {
         id_sale:sales[5].id,
         id_product:products[5].id,
-        cantidad: 1,
+        quantity: 1,
     }
 ];
 
-const revenue = [
-    { month: 'Jan', revenue: 2000 },
-    { month: 'Feb', revenue: 1800 },
-    { month: 'Mar', revenue: 2200 },
-    { month: 'Apr', revenue: 2500 },
-    { month: 'May', revenue: 2300 },
-    { month: 'Jun', revenue: 3200 },
-    { month: 'Jul', revenue: 3500 },
-    { month: 'Aug', revenue: 3700 },
-    { month: 'Sep', revenue: 2500 },
-    { month: 'Oct', revenue: 2800 },
-    { month: 'Nov', revenue: 3000 },
-    { month: 'Dec', revenue: 4800 },
-];
-
-export { categories, stock, products, detail_sale_product, sales, users, role, people, revenue };
+export { categories, stock, products, detail_sale_products, sales, users, role, people };
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,20 +1,73 @@
 import bcrypt from "bcrypt";
 import { db } from "@vercel/postgres";
 
-import {categories,stock,products,detail_sale_product,sales,users,people } from '../lib/placeholder-data';
+import {role, people, users, sales, categories, products, stock, detail_sale_products} from '../lib/placeholder-data';
 
 const client = await db.connect();
 
+async function seedRole() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS role (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
+    );
+  `;
+
+  const insertedRole = await Promise.all(
+    role.map(async (rol) => {
+      return client.sql`
+        INSERT INTO role (id, name)
+        VALUES (${rol.id}, ${rol.name})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+
+    }),
+  );
+
+  return insertedRole;
+}
+
+async function seedPeople() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS people (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      dni CHAR(8) NOT NULL,
+      name VARCHAR(255) NOT NULL,
+      lastname VARCHAR(255) NOT NULL,
+      date_register TIMESTAMP  NOT NULL)
+  `;
+
+  const insertedPeople = await Promise.all(
+    people.map(async (person) => {
+      return client.sql`
+        INSERT INTO people (id, dni, name, lastname,date_register)
+        VALUES (${person.id}, ${person.dni}, ${person.name},${person.lastname},${person.date_register.toISOString()})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+
+    }),
+  );
+
+  return insertedPeople;
+}
 
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  
+
   await client.sql`
     CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL
+      id_role UUID NOT NULL, 
+      id_people UUID NOT NULL, 
+      user_name VARCHAR(255) NOT NULL,
+      password TEXT NOT NULL,
+      date_login TIMESTAMP  NOT NULL,
+      CONSTRAINT fk_role FOREIGN KEY (id_role) REFERENCES role(id),
+      CONSTRAINT fk_people FOREIGN KEY (id_people) REFERENCES people(id)
     );
   `;
 
@@ -22,11 +75,10 @@ async function seedUsers() {
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       return client.sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        INSERT INTO users (id, id_role, id_people, user_name, password, date_login)
+        VALUES (${user.id}, ${user.id_role}, ${user.id_people}, ${user.user_name}, ${hashedPassword}, ${user.date_login.toISOString()})
         ON CONFLICT (id) DO NOTHING;
       `;
-      
     }),
   );
 
@@ -39,19 +91,19 @@ async function seedSales() {
   await client.sql`
     CREATE TABLE IF NOT EXISTS sales (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      product_id UUID NOT NULL,
-      category_id UUID NOT NULL,
-      amount INT NOT NULL,
-      method VARCHAR(255) NOT NULL,
-      date DATE NOT NULL
+      id_user UUID NOT NULL,
+      method VARCHAR(50)  NOT NULL,
+      date TIMESTAMP NOT NULL,
+      total NUMERIC(10,2) NOT NULL,
+      CONSTRAINT fk_users FOREIGN KEY (id_user) REFERENCES users(id)
     );
   `;
 
   const insertedSales = await Promise.all(
     sales.map(
       (sale) => client.sql`
-        INSERT INTO sales (product_id, category_id, amount, method, date)
-        VALUES (${sale.product_id},${sale.category_id}, ${sale.amount}, ${sale.method}, ${sale.date})
+        INSERT INTO sales (id, id_user, method, date, total)
+        VALUES (${sale.id},${sale.id_user}, ${sale.method}, ${sale.date.toISOString()}, ${sale.total})
         ON CONFLICT (id) DO NOTHING;
       `,
     ),
@@ -60,31 +112,7 @@ async function seedSales() {
   return insertedSales;
 }
 
-async function seedProducts() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-  await client.sql`
-    CREATE TABLE IF NOT EXISTS products (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      image_url VARCHAR(255) NOT NULL
-    );
-  `;
-
-  const insertedProducts = await Promise.all(
-    products.map(
-      (product) => client.sql`
-        INSERT INTO products (id, name, image_url)
-        VALUES (${product.id}, ${product.name}, ${product.image_url})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-    ),
-  );
-
-  return insertedProducts;
-}
-
-async function seedCategories(){
+async function seedCategories() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
   await client.sql`
@@ -106,47 +134,103 @@ async function seedCategories(){
   return insertedCategories
 }
 
-async function seedRevenue() {
+async function seedProducts() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
   await client.sql`
-    CREATE TABLE IF NOT EXISTS revenue (
-      month VARCHAR(4) NOT NULL UNIQUE,
-      revenue INT NOT NULL
+    CREATE TABLE IF NOT EXISTS products (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      id_category UUID NOT NULL,
+      name VARCHAR(255) NOT NULL,
+      image_url TEXT NOT NULL,
+      price NUMERIC(10,2) NOT NULL,
+      date TIMESTAMP NOT NULL,
+      CONSTRAINT fk_category FOREIGN KEY (id_category) REFERENCES categories(id)
     );
   `;
 
-  const insertedRevenue = await Promise.all(
-    revenue.map(
-      (rev) => client.sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
+  const insertedProducts = await Promise.all(
+    products.map(
+      (product) => client.sql`
+        INSERT INTO products (id, id_category, name, image_url, price, date)
+        VALUES (${product.id}, ${product.id_category},${product.name}, ${product.image_url},${product.price},${product.date.toISOString()})
+        ON CONFLICT (id) DO NOTHING;
       `,
     ),
   );
 
-  return insertedRevenue;
+  return insertedProducts;
+}
+
+async function seedStock() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+  await client.sql`
+  CREATE TABLE IF NOT EXISTS stock(
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id_product UUID NOT NULL,
+    quantity INTEGER NOT NULL,
+    date TIMESTAMP NOT NULL,
+    CONSTRAINT fk_products FOREIGN KEY (id_product) REFERENCES products(id)
+  );
+  `;
+
+  const insertedStock = await Promise.all(
+    stock.map((st) => client.sql`
+      INSERT INTO stock (id,id_product,quantity,date)
+      VALUES (${st.id}, ${st.id_product}, ${st.quantity},${st.date.toISOString()} )
+      ON CONFLICT (id) DO NOTHING
+      `
+    )
+  );
+  return insertedStock
+}
+
+async function seedDetail_Sale_Products() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+  await client.sql`
+  CREATE TABLE IF NOT EXISTS detail_sale_products(
+    id_sale UUID NOT NULL,
+    id_product UUID NOT NULL,
+    quantity INTEGER NOT NULL,
+    CONSTRAINT fk_sale FOREIGN KEY (id_sale) REFERENCES sales(id),
+    CONSTRAINT fk_products FOREIGN KEY (id_product) REFERENCES products(id)
+  );
+  `;
+
+  const inserteDetail_Sale_Products = await Promise.all(
+    detail_sale_products.map((dsp) => client.sql`
+      INSERT INTO detail_sale_products (id_sale, id_product, quantity)
+      VALUES (${dsp.id_sale}, ${dsp.id_product}, ${dsp.quantity} )
+      `
+    )
+  );
+  return inserteDetail_Sale_Products
 }
 
 export async function GET() {
-  // return Response.json({
-  //   message:
-  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  // });
+
   try {
     await client.sql`BEGIN`;
+    await seedRole();
+    await seedPeople();
     await seedUsers();
-    await seedProducts();
     await seedCategories();
+    await seedProducts();
     await seedSales();
-    await seedRevenue();
+    await seedDetail_Sale_Products();
+    await seedStock();
     await client.sql`COMMIT`;
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
     await client.sql`ROLLBACK`;
-    return Response.json({ error }, { status: 500 });
+    console.error('Error during database seeding:', error);
+    const errorMessage = (error as Error).message;
+    return Response.json({ message: 'Error during database seeding', error: errorMessage }, { status: 500 });
+   
   } finally {
-    client.release(); // Libera la conexión al pool
+    client.release(); 
   }
 }
-// la función debe llamarse GET si quieres que se ejecute cuando el cliente haga una solicitud HTTP GET.
